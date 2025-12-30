284d5b4c664fcc7e5a7b42a72e7f1d201d61170d
Calum Webb <calumpeterwebb@icloud.com>
2025-12-30 16:37:23 -0700

    feat(scripts): auto-suggest next patch version in release script
    Press enter to accept suggested version (current + 1 patch).

--- scripts/release-desktop.sh
+++ scripts/release-desktop.sh
@@ -19,10 +19,19 @@ fi
 LATEST_TAG=$(git tag -l "desktop-v*" --sort=-v:refname | head -1)
 LATEST_VERSION=${LATEST_TAG#desktop-v}
 echo "Latest release: ${LATEST_VERSION:-(none)}"
+
+# Calculate next patch version
+if [ -n "$LATEST_VERSION" ]; then
+    IFS='.' read -r MAJOR MINOR PATCH <<< "$LATEST_VERSION"
+    NEXT_VERSION="$MAJOR.$MINOR.$((PATCH + 1))"
+else
+    NEXT_VERSION="0.1.0"
+fi
 echo ""
 
-# Get new version
-read -p "New version: " VERSION
+# Get new version (with suggested default)
+read -p "New version [$NEXT_VERSION]: " VERSION
+VERSION=${VERSION:-$NEXT_VERSION}
 
 # Validate
 if ! [[ "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
