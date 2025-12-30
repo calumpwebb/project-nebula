2befa360f81e1a5244ace5c105d1c670e684b7e2
Calum Webb <calumpeterwebb@icloud.com>
2025-12-29 22:11:30 -0700

    feat(infra): migrate temporal to cdk8s manifest

---
+++ apps/temporal/deploy/manifest.ts
@@ -0,0 +1,41 @@
+// apps/temporal/deploy/manifest.ts
+
+import { app } from '../../../infra/lib'
+
+export default [
+  app('temporal-db', {
+    image: 'postgres:15',
+    labels: ['temporal'],
+    portForwards: ['5432'],
+    storage: { path: '/var/lib/postgresql/data', size: '1Gi' },
+    env: {
+      POSTGRES_USER: 'temporal',
+      POSTGRES_PASSWORD: 'temporal',
+      POSTGRES_DB: 'temporal',
+    },
+  }),
+
+  app('temporal', {
+    image: 'temporalio/auto-setup:latest',
+    labels: ['temporal'],
+    portForwards: ['7233', '7243'],
+    resourceDeps: ['temporal-db'],
+    env: {
+      DB: 'postgresql',
+      DB_PORT: '5432',
+      POSTGRES_USER: 'temporal',
+      POSTGRES_PWD: 'temporal',
+      POSTGRES_SEEDS: 'temporal-db',
+    },
+  }),
+
+  app('temporal-ui', {
+    image: 'temporalio/ui:latest',
+    labels: ['temporal'],
+    portForwards: ['8080'],
+    resourceDeps: ['temporal'],
+    env: {
+      TEMPORAL_ADDRESS: 'temporal:7233',
+    },
+  }),
+]
